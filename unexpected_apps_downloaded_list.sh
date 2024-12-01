951b2187e5516fd48eb8db051cbc5ed46bca9134
gabesterr <gabesterr@users.noreply.github.com>
2024-12-01 14:37:05 -0600

    Update unexpected_apps_downloaded_list.sh
    replaced static google document id with variable to be modified as needed

--- unexpected_apps_downloaded_list.sh
+++ unexpected_apps_downloaded_list.sh
@@ -4,6 +4,7 @@
 
 expectedFiles=/tmp/expected_apps.csv
 listFiles=/tmp/applist.txt
+googleSourceID="ENTERYOURGOOGLESOURCEIDHERE" # this is the value of your sheet after "https://docs.google.com/spreadsheets/d/" and before the next slash
 appFound=""
 
 # remove previous expected list if present
@@ -12,7 +13,7 @@ if [ -f ${expectedFiles} ]; then
 fi
 
 # get expected list 
-tmpExpectedAppsFile=$(curl -L -C - "https://docs.google.com/spreadsheets/d/1uJuK_PWNaSB0fKtexE472goEwIf7W_EsCFkcBbqBM2g/export?exportFormat=csv" -o $expectedFiles)
+tmpExpectedAppsFile=$(curl -L -C - "https://docs.google.com/spreadsheets/d/${googleSourceID}/export?exportFormat=csv" -o $expectedFiles)
 parseFile=$(/usr/bin/sed -i '' 's'/$(printf "\x0d\x0a")'/'$(printf "\x0a")'/g' $expectedFiles)
 # then remove the line endings (was getting x0dx0a vs x0a on ls -1 output below)
 # xxd $expectedFiles # checking to ensure the downloaded line endings match ls -1 line endings
